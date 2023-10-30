914430a1355c91293e052187501a8786a12ee81c
svetsitkh <svetsitkh@gmail.com>
2023-10-30 02:29:52 +0200

    task done

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,25 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min = 0;
+        this.max = 0;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.number = Math.ceil(this.min + (this.max - this.min) / 2);
+        return this.number;
     }
 
     lower() {
-
+        this.max = this.number;
     }
 
     greater() {
-
+        this.min = this.number;
     }
 }
 
